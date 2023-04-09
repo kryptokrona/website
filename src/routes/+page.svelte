6e3d1474c8fc77516178f861a584858cc56c25a5
Lukas <Lukas.nyberg@gmail.com>
2023-04-09 02:22:26 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,11 @@
 
 <svelte:head>
   <title>Kryptokrona</title>
+  <meta property="og:title" content="Kryptokrona" />
+  <meta name="twitter:title" content="Kryptokrona" />
+  <meta name="description" content="A Nordic Cryptocurrency to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the communication and money of the future." />
+  <meta name="twitter:description" content="A Nordic Cryptocurrency to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the communication and money of the future." />
+  <meta property="og:description" content="A Nordic Cryptocurrency to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the communication and money of the future." />
 </svelte:head>
 
 <div class="relative isolate">
