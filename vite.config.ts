714e360337aeb9a5f3aaaa20c0aeecf22e2f6a15
Lukas <Lukas.nyberg@gmail.com>
2023-04-10 16:44:42 +0200

    add

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
+	}
 });
