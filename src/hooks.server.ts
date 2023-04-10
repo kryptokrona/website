d7881a3c0f8627b3554c8dfd7472ae6e674d2c41
Lukas <Lukas.nyberg@gmail.com>
2023-04-10 14:30:35 +0200

    Create hooks.server.ts

---
+++ src/hooks.server.ts
@@ -0,0 +1,19 @@
+import type { Handle } from "@sveltejs/kit";
+
+const securityHeaders = {
+  'Cross-Origin-Embedder-Policy': 'require-corp',
+  'Cross-Origin-Opener-Policy': 'same-origin',
+  // [...]
+  'X-XSS-Protection': '0',
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event);
+  if ("headers" in response) {
+    Object.entries(securityHeaders).forEach(
+      ([header, value]) => response.headers.set(header, value)
+    );
+  }
+
+  return response;
+}
