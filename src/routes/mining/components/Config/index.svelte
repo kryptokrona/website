846a0781c6adda5fc129912f6467d7d3615432bc
Lukas <Lukas.nyberg@gmail.com>
2023-04-10 21:00:16 +0200

    light

--- src/routes/mining/components/Config/index.svelte
+++ src/routes/mining/components/Config/index.svelte
@@ -1,9 +1,9 @@
-<div class="py-16 sm:py-24 bg-neutral-800 sm:bg-transparent">
+<div class="py-16 sm:py-24 dark:bg-neutral-800 sm:bg-transparent">
   <div class="mx-auto max-w-6xl px-4 xl:px-2">
-    <div class="mx-auto grid grid-cols-1 gap-10 lg:grid-cols-12 lg:gap-8 p-4 sm:p-8 rounded-lg sm:bg-neutral-800 sm:border border-neutral-700">
+    <div class="mx-auto grid grid-cols-1 gap-10 lg:grid-cols-12 lg:gap-8 p-4 sm:p-8 rounded-lg dark:sm:bg-neutral-800 sm:border border-neutral-700">
       <div class="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
-        <h2 class="inline sm:block lg:inline xl:block">Generate your config file</h2>
-        <p class="inline sm:block lg:inline xl:block">with ease.</p>
+        <h2 class="inline sm:block lg:inline xl:block dark:text-neutral-50 text-neutral-900">Generate your config file</h2>
+        <p class="inline sm:block lg:inline xl:block dark:text-neutral-50 text-neutral-900">with ease.</p>
       </div>
       <form class="w-full max-w-md lg:col-span-5 lg:pt-2">
         <div class="flex gap-x-4">
@@ -13,7 +13,7 @@
             name="text"
             type="text"
             required
-            class="min-w-0 flex-auto rounded-md border-0 bg-white/10 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-white/75 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+            class="min-w-0 flex-auto rounded-md border-0 bg-white/10 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset dark:ring-white/10 ring-neutral-700 dark:placeholder:text-white/75 placeholder:text-neutral-600 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
             placeholder="Enter your wallet address"
           />
           <button
--- src/routes/wallet/components/Wallets/Wallet.svelte
+++ src/routes/wallet/components/Wallets/Wallet.svelte
@@ -11,12 +11,12 @@
 </script>
 
 <li
-	class="col-span-1 divide-y divide-neutral-600 rounded-lg bg-neutral-800 border border-neutral-700 shadow"
+	class="col-span-1 divide-y divide-neutral-700 rounded-lg dark:bg-neutral-800 border border-neutral-700 shadow"
 >
 	<div class="flex w-full items-center justify-between space-x-6 p-6">
 		<div class="flex-1 truncate">
 			<div class="flex items-center space-x-3">
-				<h3 class="truncate text-lg font-medium text-neutral-50">{title}</h3>
+				<h3 class="truncate text-lg font-medium dark:text-neutral-50">{title}</h3>
 				<span
 					class="inline-flex gap-2 flex-shrink-0 rounded-full py-0.5 text-xs font-medium dark:text-neutral-50 text-neutral-900"
 				>
@@ -34,14 +34,14 @@
 					{/if}
 				</span>
 			</div>
-			<p class="mt-1 truncate text-sm text-neutral-400">{text}</p>
+			<p class="mt-1 truncate text-sm dark:text-neutral-400 text-neutral-700">{text}</p>
 		</div>
 	</div>
 	<div>
 		<div class="-mt-px flex divide-x divide-neutral-600">
 			<div class="flex w-0 flex-1">
 				<a
-					class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-neutral-50"
+					class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold dark:text-neutral-50"
 					href={download}
 				>
 					<DownloadIcon />
@@ -50,7 +50,7 @@
 			</div>
 			<div class="-ml-px flex w-0 flex-1">
 				<a
-					class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-neutral-50"
+					class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold dark:text-neutral-50"
 					href={source}
 				>
 					<GithubIcon />
