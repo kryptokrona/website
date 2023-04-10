6804f2dbe388301b0ebf18daa35e1bcc43e0643c
Lukas <Lukas.nyberg@gmail.com>
2023-04-10 18:48:38 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,25 +3,37 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Signup from '$lib/components/Signup.svelte';
 	import { blogPosts, nodeData, supplyData } from '$lib/stores/data';
-  import { dev } from '$app/environment';
-  import { inject } from '@vercel/analytics';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import { Toaster } from 'svelte-french-toast';
+	import { fade } from 'svelte/transition';
 
-  import '../app.css';
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { quadInOut } from 'svelte/easing';
 
 	export let data;
+	let startAnimation;
 
 	nodeData.set(data.node);
 	blogPosts.set(data.posts);
 	supplyData.set(data.supply);
 
-  inject({ mode: dev ? 'development' : 'production' });
+	inject({ mode: dev ? 'development' : 'production' });
 
+	onMount(() => {
+		startAnimation = true;
+	});
 </script>
 
 <Toaster />
 
 <Navbar />
-<slot />
+{#key $page.url.href}
+	<main in:fade={{ duration: 500, easing: quadInOut }}>
+		<slot />
+	</main>
+{/key}
 <Signup />
 <Footer />
