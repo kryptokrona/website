cb98351dcebe25c41a74cc56189b12c24ba6e322
Lukas <Lukas.nyberg@gmail.com>
2023-04-10 16:36:22 +0200

    test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,5 @@
 import type { Handle } from "@sveltejs/kit";
+import * as cookie from 'cookie';
 
 const securityHeaders = {
   'Cross-Origin-Embedder-Policy': 'require-corp',
@@ -8,10 +9,17 @@ const securityHeaders = {
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
+  const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+  event.locals.userid = cookies['userid'] || crypto.randomUUID();
+
   const response = await resolve(event);
+
   if ("headers" in response) {
     Object.entries(securityHeaders).forEach(
       ([header, value]) => response.headers.set(header, value)
+    );
+    response.headers.set(
+      'set-cookie', cookie.serialize('userid', event.locals.userid, { path: '/', httpOnly: true })
     );
   }
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,29 @@
 <script>
-	import '../app.css';
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Signup from '$lib/components/Signup.svelte';
 	import { blogPosts, nodeData, supplyData } from '$lib/stores/data';
 	import { Toaster } from 'svelte-french-toast';
+  import { browser } from "$app/environment";
+  import { webVitals } from '$lib/vitals';
+  import { page } from '$app/stores';
+
+  import '../app.css';
 
 	export let data;
 
 	nodeData.set(data.node);
 	blogPosts.set(data.posts);
 	supplyData.set(data.supply);
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId
+    })
+  }
 </script>
 
 <Toaster />
