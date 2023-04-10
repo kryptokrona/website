33c6f156c3399019b40660e31a0847619f4edc70
Lukas <Lukas.nyberg@gmail.com>
2023-04-10 21:52:54 +0200

    dynamic sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,25 +1,42 @@
 import type { RequestHandler } from '@sveltejs/kit'
+import { getDirectusClient } from "../../lib/utils/directus";
+
+let blogPosts: ManyItems<unknown, { fields: string[] }> = []
+const directus = await getDirectusClient();
+
+
+blogPosts = await directus.items('posts').readByQuery({
+    fields: ['slug'],
+  }) ?? []
 
 export const GET: RequestHandler = async () => {
-  const body = `<?xml version="1.0" encoding="UTF-8"?>
+  if (!blogPosts.data) return
+    const body = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
             <loc>https://kryptokrona.org</loc>
-            <changefreq>daily</changefreq>
+            <changefreq>weekly</changefreq>
         </url>
         <url>
             <loc>https://kryptokrona.org/about</loc>
-            <changefreq>daily</changefreq>
+            <changefreq>weekly</changefreq>
         </url>
         <url>
             <loc>https://kryptokrona.org/mining</loc>
-            <changefreq>daily</changefreq>
+            <changefreq>weekly</changefreq>
         </url>
         <url>
             <loc>https://kryptokrona.org/wallet</loc>
-            <changefreq>daily</changefreq>
+            <changefreq>weekly</changefreq>
         </url>
-    </urlset>`
+                ${blogPosts.data.map((data: any) =>
+      `<url>
+            <loc>https://kryptokrona.org/${data.slug}</loc>
+            <changefreq>weekly</changefreq>
+        </url>`
+    )
+      .join("")}
+    </urlset>`;
 
   return new Response(String(body), {
     status: 200,
@@ -27,4 +44,5 @@ export const GET: RequestHandler = async () => {
       'Content-Type': 'application/xml',
     },
   })
+
 }
