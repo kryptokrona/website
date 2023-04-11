31560dcee39417e6bb4338317d852aaae1ca9a79
Lukas <Lukas.nyberg@gmail.com>
2023-04-11 10:05:04 +0200

    Update Header.svelte

--- src/routes/wallet/components/Header.svelte
+++ src/routes/wallet/components/Header.svelte
@@ -1,13 +1,13 @@
 <div class="pt-32 sm:pt-36 lg:pt-40 pb-16 sm:pb-20">
   <div class="mx-auto max-w-6xl">
     <div class="mx-auto px-4 xl:px-2">
-      <p class="text-base font-semibold leading-7 dark:text-neutral-300 text-neutral-600">Store your funds</p>
+      <p class="text-base font-semibold leading-7 dark:text-neutral-300 text-neutral-600">Your new bank</p>
       <h2 class="mt-2 text-4xl font-bold tracking-tight text-neutral-900 dark:text-neutral-50 sm:text-6xl">Wallets</h2>
-      <p class="mt-6 text-lg leading-8 dark:text-neutral-400 text-neutral-600">Kryptokrona is a cryptocurrency that utilizes proof-of-work mining to establish distributed consensus. The information and resources provided below will assist you in getting started with mining. Please note that the Kryptokrona Project does not endorse any specific pool, software, or hardware; the content presented here is for informational purposes only.</p>
+      <p class="mt-6 text-lg leading-8 dark:text-neutral-400 text-neutral-600">In a rapidly evolving digital world, self-sovereignty has never been more crucial. As we break free from the shackles of traditional financial systems, cryptocurrency wallets empower us to reclaim control over our wealth and forge a path towards financial independence.</p>
 
-      <p class="mt-6 text-lg leading-8 dark:text-neutral-400 text-neutral-600">A core tenet of Kryptokrona's philosophy is to promote equitable mining opportunities for everyone. To accomplish this, Kryptokrona employs a unique algorithm conceived and developed by members of the Kryptokrona community: RandomX. This proof-of-work algorithm is designed to be ASIC-resistant, making it infeasible for specialized hardware to mine Kryptokrona. Consequently, miners are required to utilize consumer-grade hardware and engage in fair competition.</p>
+      <p class="mt-6 text-lg leading-8 dark:text-neutral-400 text-neutral-600">The emergence of decentralized digital currencies has revolutionized the way we perceive and interact with money, challenging the age-old notion of centralized control. Cryptocurrency wallets are a testament to this transformation, serving as the conduit through which we access the limitless potential of blockchain technology.</p>
 
-      <p class="mt-6 text-lg leading-8 dark:text-neutral-400 text-neutral-600">Kryptokrona can be mined using both CPUs and GPUs; however, CPU mining is significantly more efficient in this context. Join the Kryptokrona mining community and contribute to the future of decentralized digital currency.</p>
+      <p class="mt-6 text-lg leading-8 dark:text-neutral-400 text-neutral-600">With a cryptocurrency wallet in hand, we are no longer mere participants in a financial system dictated by centralized institutions. Instead, we become the architects of our own destiny, transcending borders and barriers to connect with like-minded individuals across the globe. The power of decentralization is at the heart of this movement, with each wallet acting as a personal gateway to a world where financial freedom is not just a dream, but a tangible reality.</p>
     </div>
   </div>
 </div>
