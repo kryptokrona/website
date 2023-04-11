7f72b6868b9e00e1f829620de8ecd6306dc826b9
Lukas <Lukas.nyberg@gmail.com>
2023-04-11 10:49:25 +0200

    Update Stats.svelte

--- src/routes/mining/components/Stats.svelte
+++ src/routes/mining/components/Stats.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="mx-auto max-w-6xl px-4 xl:px-2">
-  <h3 class="text-base font-semibold leading-6 text-gray-900">Network</h3>
+  <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-neutral-50">Network</h3>
   <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
     <div class="overflow-hidden rounded-lg dark:bg-neutral-800 border border-neutral-700 px-4 py-5 shadow sm:p-6">
       <dt class="truncate text-sm font-medium dark:text-gray-50">Hashrate</dt>
