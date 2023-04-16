36684f073a23265801da6b5f9f446613a4238f1e
Lukas <Lukas.nyberg@gmail.com>
2023-04-17 01:12:29 +0200

    Update +page.svelte

--- src/routes/wallet/+page.svelte
+++ src/routes/wallet/+page.svelte
@@ -2,8 +2,10 @@
   import Header from "./components/Header.svelte";
   import Wallets from "./components/Wallets/index.svelte";
   import Statusbar from "$lib/components/Statusbar.svelte";
+  import ComingSoon from "./components/ComingSoon.svelte";
 </script>
 
 <Header />
 <Wallets />
+<ComingSoon />
 <Statusbar />
