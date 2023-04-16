c6c80300e4859ba4673f7141325c0764e5ece090
Lukas <Lukas.nyberg@gmail.com>
2023-04-17 01:39:24 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import Features from "$lib/components/home/Features.svelte";
   import Header from "$lib/components/home/Header.svelte";
   import OrganisationLdJson from "$lib/seo/OrganisationLdJson.svelte";
+  import { page } from "$app/stores";
 </script>
 
 <svelte:head>
@@ -17,6 +18,7 @@
   <meta property="og:image" content="https://user-images.githubusercontent.com/36674091/230983427-7614033e-db05-4ea8-aa1e-8d4e3ca57144.png">
   <meta property="og:url" content="https://kryptokrona.org"/>
   <meta property="og:locale" content="en"/>
+  <link rel="canonical" href="{$page.url.href}" />
 </svelte:head>
 
 <!--SEO-->
