bcc54c7b640dbf099ff977e6ae251d30fea1fb66
Lukas <Lukas.nyberg@gmail.com>
2023-04-17 01:15:23 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import Footer from '$lib/layout/Footer.svelte';
 	import Navbar from '$lib/layout/Navbar.svelte';
 	import Signup from '$lib/layout/Signup.svelte';
-  import { blogPosts, nodeData, supplyData, userData } from "$lib/stores/data";
+  import { blogPosts, nodeData, supplyData } from "$lib/stores/data";
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 	import { Toaster } from 'svelte-french-toast';
@@ -26,7 +26,7 @@
   nodeData.set(data.node);
   blogPosts.set(data.posts);
   supplyData.set(data.supply);
-  userData.set(data.user)
+  // userData.set(data.user)
 
   onMount(async () => {
     nodeData.set(await fetchNode(CONFIG.NODE_ONE, CONFIG.NODE_TWO));
