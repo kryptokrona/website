80c87594b48c83b63911408718d95e1d8a051ee1
Lukas <Lukas.nyberg@gmail.com>
2023-05-09 19:14:10 +0200

    Update helpers.ts

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,6 +1,4 @@
 export const fetchNode = async (url1: string, url2: string): Promise<NodeData> => {
-	if(!url1 || !url2) return
-
 	return await Promise.any([
 		fetch(url1).then(res => res.json()),
 		fetch(url2).then(res => res.json()),
