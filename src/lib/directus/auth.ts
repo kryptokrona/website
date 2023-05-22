83663871aba471a0ad1e5b3a6487d77a618eeae7
Lukas <Lukas.nyberg@gmail.com>
2023-05-22 18:40:43 +0200

    Update auth.ts

--- src/lib/directus/auth.ts
+++ src/lib/directus/auth.ts
@@ -5,7 +5,7 @@ export const authUser = async (event: RequestEvent): Promise<User | null> => {
 
 	const userToken = await cookies.get('session');
 
-	if (userToken === 'd676468f-b7cd-4997-aa8d-fde13cecf55d') {
+	if (userToken === '') {
     return {
       id: 123,
       email: 'Email',
