c7e5a0ab73cd08a48e858248a7a0a05610a26bdb
Lukas <Lukas.nyberg@gmail.com>
2023-11-22 23:33:30 +0100

    Add FAQ about

--- src/routes/about/components/Faq/questions.ts
+++ src/routes/about/components/Faq/questions.ts
@@ -3,4 +3,24 @@ export const questions: FaqData[] = [
     question: "How can I get started with Kryptokrona?",
     answer: "To get started with Kryptokrona, you'll need to download our wallet from our website. Once you've installed the wallet, you can start receiving, sending, and storing kryptokrona. You can obtain coins by mining, purchasing them on an exchange, or receiving them from another user."
   },
+  {
+    question: "What algorithm does Kryptokrona use?",
+    answer: "Kryptokrona currently runs on CryptoNight-Pico, a more equitable algorithm than Bitcoin's SHA256. It's efficient enough to be mined even on ARM-based phones."
+  },
+  {
+    question: "How often are new blocks processed in the Kryptokrona network?",
+    answer: "New transactions are processed every 90 seconds in the Kryptokrona network, which is significantly faster compared to Bitcoin's 10-minute interval."
+  },
+  {
+    question: "How can I get involved with Kryptokrona?",
+    answer: "You can join the Kryptokrona community by contributing to its development, testing tools, or participating in discussions on platforms like Twitter, Telegram, Github, Discord, and Hugin."
+  },
+  {
+    question: "How can I mine Kryptokrona?",
+    answer: "You can join the Kryptokrona mining community to leverage your computer's processing power for validating transactions and securing the network. Mining Kryptokrona can also be a source of income."
+  },
+  {
+    question: "How can I get started with Kryptokrona?",
+    answer: "Kryptokrona offers a range of benefits, including the ability to manage your finances independently, secure and private communication through blockchain-based messaging, and participation in a community-driven, innovative cryptocurrency project."
+  },
 ]
