655b9ed0efa5501094ac8967d34255208e60d300
Lukas <Lukas.nyberg@gmail.com>
2023-11-22 23:33:21 +0100

    Add FAQ mining

--- src/routes/mining/components/Faq/questions.ts
+++ src/routes/mining/components/Faq/questions.ts
@@ -1,6 +1,26 @@
 export const questions: FaqData[] = [
   {
-    question: "How can I get started with Kryptokrona?",
-    answer: "To get started with Kryptokrona, you'll need to download our wallet from our website. Once you've installed the wallet, you can start receiving, sending, and storing kryptokrona. You can obtain coins by mining, purchasing them on an exchange, or receiving them from another user."
+    question: "What is Kryptokrona mining and how does it work?",
+    answer: "Kryptokrona mining is the process of using your computer's processing power to validate transactions and maintain the network. Miners solve complex cryptographic puzzles, and in return, they are rewarded with Kryptokrona coins. This process secures the network and ensures the integrity of transaction records."
+  },
+  {
+    question: "What are the requirements to start mining Kryptokrona?",
+    answer: "To set up your mining rig, you first need to download and install a mining software compatible with the CryptoNight-Pico algorithm. After installation, configure the miner with your Kryptokrona wallet address and choose a mining pool to join. Finally, start the mining software to begin mining."
+  },
+  {
+    question: "How can I set up my mining rig for Kryptokrona?",
+    answer: "To set up your mining rig, you first need to download and install a mining software compatible with the CryptoNight-Pico algorithm. After installation, configure the miner with your Kryptokrona wallet address and choose a mining pool to join. Finally, start the mining software to begin mining."
+  },
+  {
+    question: "Is it profitable to mine Kryptokrona?",
+    answer: "The profitability of mining Kryptokrona depends on various factors, including the efficiency of your mining rig, electricity costs, and the current market value of Kryptokrona. You can use online mining calculators to estimate your potential earnings."
+  },
+  {
+    question: "What is a mining pool, and should I join one?",
+    answer: "A mining pool is a group of miners who combine their computational power to increase their chances of earning mining rewards. The rewards are then distributed among pool members based on their contributed processing power. Joining a mining pool can offer more consistent rewards compared to mining alone, especially if you have less powerful equipment."
+  },
+  {
+    question: "How does Kryptokrona ensure fair mining opportunities?",
+    answer: "Kryptokrona uses the CryptoNight-Pico algorithm, which is designed to be more equitable than algorithms like Bitcoin's SHA256. It allows for competent mining on a variety of devices, including less powerful hardware, providing fairer access to mining opportunities for individuals."
   },
 ]
