ae7b69b6c3ba851bf4faa47618adb9c2e7570003
Lukas <Lukas.nyberg@gmail.com>
2023-12-27 12:49:58 +0100

    Fix types

--- src/app.d.ts
+++ src/app.d.ts
@@ -20,39 +20,42 @@ declare global {
 	}
 
 	interface NodeData {
-		"alt_blocks_count": number,
-		"difficulty": number,
-		"grey_peerlist_size": number,
-		"hashrate": number,
-		"height": number,
-		"incoming_connections_count": number,
-		"last_known_block_index": number,
-		"major_version": number,
-		"minor_version": number,
-		"network_height": number,
-		"outgoing_connections_count": number,
-		"start_time": number,
-		"status": string,
-		"supported_height": number,
-		"synced": boolean,
-		"testnet": boolean,
-		"tx_count": number,
-		"tx_pool_size": number,
-		"upgrade_heights": Array<any>,
-		"version": string,
-		"white_peerlist_size": number
+		alt_blocks_count: number,
+		difficulty: number,
+		grey_peerlist_size: number,
+		hashrate: number,
+		height: number,
+		incoming_connections_count: number,
+		last_known_block_index: number,
+		major_version: number,
+		minor_version: number,
+		network_height: number,
+		outgoing_connections_count: number,
+		start_time: number,
+		status: string,
+		supported_height: number,
+		synced: boolean,
+		testnet: boolean,
+		tx_count: number,
+		tx_pool_size: number,
+		upgrade_heights: Array<any>,
+		version: string,
+		white_peerlist_size: number
 	}
 
 	interface SupplyData {
-		"lastCheck": number,
-		"circulatingUnits": number,
+		lastCheck: number,
+		name: string
+		ticker: string
+		circulatingUnits: number,
 		latestBlockRewardUnits: number
-		"maxUnits": number,
-		"coinUnits": number,
-		"decimals": number,
-		"calculatedSupply": number,
-		"calculatedMaxSupply": number
-		"calculatedReward": number
+		maxUnits: number,
+		coinUnits: number,
+		decimals: number,
+		calculatedSupply: number,
+		calculatedMaxSupply: number
+		calculatedLastReward: number
+
 	}
 
 	interface WalletsData {
